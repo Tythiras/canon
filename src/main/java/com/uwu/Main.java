fe56578586cd6abf1b8eb9d74f4f7e4eeedc2c6d
Rasmus Tobiasen <rasmus.tobiasen@bbsure.dk>
2019-08-29 13:20:32 +0200

    testtt

---
+++ src/main/java/com/uwu/Main.java
@@ -0,0 +1,24 @@
+package com.uwu;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("com.uwu.Main");
+    }
+
+    public void settings(){
+        size(300,300);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        ellipse(width/2,height/2,second(),second());
+    }
+
+
+}
